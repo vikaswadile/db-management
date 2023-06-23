d9a2f29a57d2898d34ada61366c68ed34ee0b61a
vikaswadile <100255449+vikaswadile@users.noreply.github.com>
2023-06-23 23:14:31 +0530

    Create db_access_03.sh

---
+++ db_access_03.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+
+#this shell script is to clone repo update the file & push the changes made
+#author : Vikas 
+#written on : 14th-Apr-2023
+
+
+
+country=""
+env_name=""
+reason=""
+
+
+while [[ ! $country =~ ^(kenya|rwanda)$ ]]; do
+    read -p "Enter a country (kenya/rwanda): " country
+    echo "Enter country name either kenya or rwanda"
+done
+
+while [[ ! $env_name =~ ^(prod|stage)$ ]]; do
+    read -p "Please enter environment (prod/stage) : " env_name
+    echo "Enter environment either prod or stage"
+done
+
+while [[ -z $reason ]]; do
+    read -p "Please enter reference Jira ticket or mention reason : " reason
+    echo "please enter reason, do not keep it blank"
+done
+
+
+country=$country
+env=$env_name
+commit_reason=$reason
+
+if [ -d "db-management" ]; then
+  cd db-management
+  # pull the latest changes from the remote branch
+
+echo "Repository already exist locally, pulling latest changes from remote repository"
+
+  git pull origin main
+  cd ../db-management/var/${country}/${env}/
+
+else
+  # clone the repository
+
+echo "Cloning remote repository to local !"
+
+sleep 2
+
+  git clone git@github.com:vikaswadile/db-management.git
+  cd db-management/var/${country}/${env}/
+fi
+
+
+echo "--------------------------------------------------"
+echo "Below is the present working directory"
+echo "--------------------------------------------------"
+
+pwd
+
+#listing files present in the directory
+sleep 1
+echo "--------------------------------------------------"
+echo "Below are the files present in repository" 
+echo "--------------------------------------------------"
+
+ls -1p | grep -v / | awk '{print $1}'
+
+# prompt the user for the file to update
+sleep 1
+echo "--------------------------------------------------"
+read -p "Enter the name of the file you want to update: " file_names
+echo "--------------------------------------------------"
+sleep 1
+
+# edit the file
+
+echo "---------------------------------------------------"
+echo "you have selected $file_name,please wait opening vi editor for you."
+echo "---------------------------------------------------"
+sleep 1
+
+for file in $file_names
+do
+vim $file
+done
+
+
+echo "---------------------------------------------------"
+echo "file $file_name is updated & saved succesfully."
+echo "---------------------------------------------------"
+sleep 1
+
+
+# git difference
+
+echo "---------------------------------------------------"
+git status
+echo "---------------------------------------------------"
+
+echo "---------------------------------------------------"
+git diff
+echo "---------------------------------------------------"
+
+read -p "Above changes looks good to you? (yes/no): " choice
+
+# Check if the user wants to proceed or not
+if [ "$choice" == "yes" ] || [ "$choice" == "Yes" ]; then
+    # Proceed with the steps here
+
+# commit the changes
+
+git add *
+git commit -m "Updated $file_names : Reason--> $commit_reason "
+
+# push the changes to the remote branch
+sleep 1
+echo "---------------------------------------------------"
+echo "Pushing changes to the remote repository"
+echo "---------------------------------------------------"
+
+git push origin main
+
+#if push operation failed
+
+if [ $? -ne 0 ]; then
+    echo "---------------------------------------------------"
+    echo "Push operation failed! The remote repository may have the latest changes and commits."
+    echo "---------------------------------------------------"
+    
+        cd ../../../../
+        rm -rf db-management    
+
+    read -p "Do you want to re-execute this script to avoid conflicts? (yes/no): " retry_choice
+    if [ "$retry_choice" == "yes" ] || [ "$retry_choice" == "Yes" ]; then
+      exec "$0"
+    else
+      exit 1
+    fi
+
+  else
+    echo "----------------------------------------------------"
+    echo "Your changes were pushed successfully to the remote repository."
+    echo "----------------------------------------------------"
+
+    cd ../../../../
+    rm -rf db-management    
+
+
+
+  fi
+
+    # more steps here
+else
+    # Exit the script if the user does not want to proceed
+    cd ../../../../
+    rm -rf db-management
+    echo "Exiting the script..."
+    exit 1
+fi
